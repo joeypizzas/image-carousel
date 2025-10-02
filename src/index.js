7a8f306f16d3363d7d5166edc7de8f82aed48d44
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-10-02 18:51:15 -0400

    Instantiated image objects

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 
 import "./style.css";
 import { initCarouselListeners } from "./carouselView.js";
+import garlicKnotsImage from "./assets/garlic_knots.png";
+import meatballsImage from "./assets/meatballs.png";
+import tomatoPieImage from "./assets/tomato_pie.png";
+import mozzarellaImage from "./assets/mozz_pie.png";
+import pepperoniImage from "./assets/pepperoni_pie.png";
+import { CarouselImage, images } from "./carousel.js";
+
+const mozzPie = new CarouselImage(mozzarellaImage, "Mozz pie");
+const pepperoniPie = new CarouselImage(pepperoniImage, "Pepperoni pie");
+const tomatoPie = new CarouselImage(tomatoPieImage, "Tomato pie");
+const meatballs = new CarouselImage(meatballsImage, "Meatballs");
+const garlicKnots = new CarouselImage(garlicKnotsImage, "Garlic knots");
+
+images.addNewImage(mozzPie);
+images.addNewImage(pepperoniPie);
+images.addNewImage(tomatoPie);
+images.addNewImage(meatballs);
+images.addNewImage(garlicKnots);
 
 initCarouselListeners();
