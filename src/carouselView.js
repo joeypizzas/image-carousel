7579cc7864fd493a6d0323da8e455903e6d70982
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-10-03 08:10:38 -0400

    finished writing function to add images to UI

--- src/carouselView.js
+++ src/carouselView.js
@@ -1,9 +1,10 @@
 // JS for carousel view
 
-import { CarouselImage, images } from "./carousel.js";
+import { images } from "./carousel.js";
+
+const root = document.documentElement;
 
 export function initCarouselListeners() {
-  const root = document.documentElement;
   const arrowButtons = document.querySelectorAll(".arrow-button");
   const dotButtons = document.querySelectorAll(".dot-button");
 
@@ -55,34 +56,47 @@ export function initCarouselListeners() {
       }
     });
   });
+}
 
-  function selectDot() {
-    const newSelectedDot = document.querySelector(".selected-dot");
-    const newSelectedDotSVG = newSelectedDot.querySelector(".dot-svg");
-    newSelectedDotSVG.style.stroke =
-      getComputedStyle(root).getPropertyValue("--button-hover");
-    newSelectedDotSVG.style.fill =
-      getComputedStyle(root).getPropertyValue("--button-hover");
-  }
+function selectDot() {
+  const newSelectedDot = document.querySelector(".selected-dot");
+  const newSelectedDotSVG = newSelectedDot.querySelector(".dot-svg");
+  newSelectedDotSVG.style.stroke =
+    getComputedStyle(root).getPropertyValue("--button-hover");
+  newSelectedDotSVG.style.fill =
+    getComputedStyle(root).getPropertyValue("--button-hover");
+}
 
-  function unselectDot() {
-    const formerSelectedDot = document.querySelector(".selected-dot");
-    if (formerSelectedDot) {
-      const formerSelectedDotSVG = formerSelectedDot.querySelector(".dot-svg");
-      formerSelectedDotSVG.style.fill =
-        getComputedStyle(root).getPropertyValue("--page-background");
-      formerSelectedDotSVG.style.stroke =
-        getComputedStyle(root).getPropertyValue("--button");
-      formerSelectedDot.classList.remove("selected-dot");
-    }
+function unselectDot() {
+  const formerSelectedDot = document.querySelector(".selected-dot");
+  if (formerSelectedDot) {
+    const formerSelectedDotSVG = formerSelectedDot.querySelector(".dot-svg");
+    formerSelectedDotSVG.style.fill =
+      getComputedStyle(root).getPropertyValue("--page-background");
+    formerSelectedDotSVG.style.stroke =
+      getComputedStyle(root).getPropertyValue("--button");
+    formerSelectedDot.classList.remove("selected-dot");
   }
 }
 
 export function addCarouselImagesToUI(carouselImageArr) {
   const imagesToShow = carouselImageArr;
   const carouselImgs = document.querySelectorAll(".carousel-img");
+  const dotButtons = document.querySelectorAll(".dot-button");
 
-  for (imgs of carouselImgs) { // figure out how to get index of image in the nodelist to get correct image from array
-    img.src = 
-  }
+  carouselImgs.forEach((img, i) => {
+    img.src = imagesToShow[i].src;
+    img.alt = imagesToShow[i].alt;
+    img.dataset.id = imagesToShow[i].id;
+  });
+
+  dotButtons.forEach((dot, i) => {
+    dot.dataset.id = imagesToShow[i].id;
+    if (i === 2) {
+      if (!dot.classList.contains("selected-dot")) {
+        dot.classList.add("selected-dot");
+        selectDot();
+      }
+    }
+  });
 }
